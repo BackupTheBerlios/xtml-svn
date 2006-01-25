4b26e12837b4a6223a7a5c7d8780ec549613d838
jallen <jallen@7b2debec-9f0b-0410-9bf7-b1e043166da1>
2006-01-25 19:27:26 +0000

    Doc updates

--- main/gendoc.sh
+++ main/gendoc.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
 
 STYLE=${STYLE:-"HTML:frames:DOM/earthli"}
+FILES="src/cTagLib.class.php,src/stringTagLib.class.php,src/eTagLib.class.php,src/htmlTagLib.class.php,src/i18nTagLib.class.php,src/mdTagLib.class.php,src/phpTagLib.class.php"
 
 # Remove the existing documentation
 find documentation/taglibs -type f | grep -v "\.svn" | xargs rm -vf
 
 # Create new documentation
-phpdoc -f 'src/*TagLib.class.php' --title "PiSToL tag libraries documentation" \
+phpdoc -f $FILES --title "PiSToL tag libraries documentation" \
 	--output $STYLE -t documentation/taglibs
 	
 # Replace _colon_ with : in the generated documentation	
